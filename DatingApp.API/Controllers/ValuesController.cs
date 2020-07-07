476d303e79a9656fb975d4a69014e272fbac4293
MalithW1995 <malith.sandagiri@outlook.com>
2020-07-07 16:11:52 +0530

    Get data from DB and async await

--- DatingApp.API/Controllers/ValuesController.cs
+++ DatingApp.API/Controllers/ValuesController.cs
@@ -2,7 +2,9 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using DatingApp.API.Data;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace DatingApp.API.Controllers
 {
@@ -10,18 +12,31 @@ namespace DatingApp.API.Controllers
     [ApiController]
     public class ValuesController : ControllerBase
     {
-        // GET api/values
-        [HttpGet]
-        public ActionResult<IEnumerable<string>> Get()
+        private readonly DataContext _context;
+        public ValuesController(DataContext context)
         {
-            return new string[] { "value1", "value2" };
-        }
+            _context = context;
 
+        }
+        // GET api/values
+        // [HttpGet]
+        // public IActionResult GetValues()
+        // {
+        //     var values = _context.Values.ToList();
+        //     return Ok(values);
+        // }
+        [HttpGet]
+        public async Task<IActionResult> GetValues()
+        {
+            var values = await _context.Values.ToListAsync();
+            return Ok(values);
+        }
         // GET api/values/5
         [HttpGet("{id}")]
-        public ActionResult<string> Get(int id)
+        public async Task<IActionResult> GetValue(int id)
         {
-            return "value";
+            var value = await _context.Values.FirstOrDefaultAsync(x => x.id == id); // if not matched send default value as null
+            return Ok(value);
         }
 
         // POST api/values
